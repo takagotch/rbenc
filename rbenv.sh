122cf9b089b4b4a94fbb1e4954e95ea96d56afc1
takagotch <dyaccb@gmail.com>
2017-11-16 01:16:44 +0900

    rbenv.sh

---
+++ rbenv.sh
@@ -0,0 +1,19 @@
+
+sudo yum -y install git
+git clone https:github.com/sstephenson/rbenb.git ~/.rbenv
+git clone https:github.com/sstepheson/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+source ~/.bash_profile
+rbenv --version
+rbenv install --list
+rbenv install 2.1.2
+rbnev versions
+mkdir app
+cd app
+rbenv local 2.1.2
+cd ~/.rbenv
+git pull
+cd ~/.rbenv/plugins/ruby-build
+git pull
+
